8ac15be78051414f5566118b8f3197eba265a993
Brent Gawryluik <brentwg@zoho.com>
2018-03-05 21:39:03 -0800

    Added install ansible code

---
+++ setup-everything.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# ------------------------------------------------------------------------
+# Wrapper script used to bootstrap, install, and configure everything 
+# required to execute this playbook.
+#
+# Brent WG
+# 2018-03-05
+# ------------------------------------------------------------------------
+
+# ----------------
+# Script Variables
+# ----------------
+INSTALL_ANSIBLE_URL="https://gist.githubusercontent.com/brentwg/adf4625a61c5ee6ce15749cb772df974/raw/"
+INSTALL_ANSIBLE_SCRIPT="${PWD}/scripts/install-ansible.sh"
+
+# ---------------
+# Install Ansible
+# ---------------
+wget -O ${INSTALL_ANSIBLE_SCRIPT} ${INSTALL_ANSIBLE_URL}
+chmod +x ${INSTALL_ANSIBLE_SCRIPT}
+sudo ${INSTALL_ANSIBLE_SCRIPT}
+
+# --------------------
+# Install Requirements
+# --------------------
+
+# --------------------
+# Execute the playbook
+# --------------------
